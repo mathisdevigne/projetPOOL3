dd1aa2a377a951e42f3e06a719a2276b23b3c42c
mathisdevigne <mathisdevigne8@gmail.com>
2022-11-17 08:47:37 +0100

    Init game&world

---
+++ colossalBlitzcronkAdventure/src/colossalblitzcronkadventure/Game.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colossalblitzcronkadventure;
+
+/**
+ *
+ * @author mathi
+ */
+public class Game {
+    private World world;
+    
+    public Game(){
+        this.world = World.get();
+    }
+}
