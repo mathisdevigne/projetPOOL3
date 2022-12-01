54e3c90bb844150f2bf9afc668862714d7a2c211
Dusehu Théo <theo.dusehu@sfr.fr>
2022-12-01 11:17:51 +0100

    Update CommandParser.java
    Update description of use command in help description

--- colossalBlitzcronkAdventure/src/colossalblitzcronkadventure/command/CommandParser.java
+++ colossalBlitzcronkAdventure/src/colossalblitzcronkadventure/command/CommandParser.java
@@ -61,7 +61,7 @@ public interface CommandParser {
     
     public static void help(List<String> command){
         if(command.size()== 1){
-            System.out.println("HELP :\nGO <arg> : Move to the specified locaton.\nLOOK <arg1> <arg2>... : Look at specified arguments\nQUIT : Quit the game\nTAKE <arg> : Take the specified item and put it in the inventory.\nUSE <arg1> <arg2> : Use the first item specified with the second. The first item needs to be in your inventory.\nFIGHT <arg> : Get into a fight with the specified enemy.\nWhile in fight you can use the command :\nATTACK : To attack the enemy.\nHEAL : To heal yourself.\nLEAVES : To leave the fight.\n Good luck !\n");
+            System.out.println("HELP :\nGO <arg> : Move to the specified locaton.\nLOOK <arg1> <arg2>... : Look at specified arguments\nQUIT : Quit the game\nTAKE <arg> : Take the specified item and put it in the inventory.\nUSE <arg1> : Use the specified item. The item need to be in your inventory.\nUSE <arg1> <arg2> : Use the first item specified with the second. Items needs to be in your inventory.\nFIGHT <arg> : Get into a fight with the specified enemy.\nWhile in fight you can use the command :\nATTACK : To attack the enemy.\nHEAL : To heal yourself.\nLEAVES : To leave the fight.\n Good luck !\n");
         }
     }
 
